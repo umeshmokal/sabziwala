3a1307cf71a8fcff4c9f8c8609ee65fb5f38d704
umeshmokal <umeshmokal08@gmail.com>
2021-04-16 20:45:00 +0530

    deploy application on heroku

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sabziwala'));
+app.use(express.static('./dist/sabziwala'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/sabziwala/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/sabziwala/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
